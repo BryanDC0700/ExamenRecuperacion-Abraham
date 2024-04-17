405cf72c77b667a34fb6a3a28194d0e82c524aa6
Abraham Brian Diaz Condori <202013239d@utea.edu.pe>
2024-04-17 12:35:09 -0500

    capa de logica de negocios y Calcular precio por persona

---
+++ logica negocio/CalculadoraVentas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenRecuperacion_Abraham
+{
+public class CalculadoraVentas
+{
+    public static void CalcularMontosVenta(char tipoCliente, int cantidadPersonas, int tipoRuta,
+        out float importeBruto, out float porcentajeDescuento, out float montoDescuento, out float importePago)
+    {
+        float[] precios = { 100, 120, 150 };
+
+        float precioPorPersona = precios[tipoRuta - 1];
+
+        
+    }
+}
+
+}
