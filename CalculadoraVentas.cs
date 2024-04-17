dc511cfb3fb1088301532323d47a42d9eebb7d89
Abraham Brian Diaz Condori <202013239d@utea.edu.pe>
2024-04-17 12:36:47 -0500

    ultima parte de logica de negocio

---
+++ CalculadoraVentas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenRecuperacion_Abraham
+{
+    public class CalculadoraVentas
+    {
+        public static void CalcularMontosVenta(char tipoCliente, int cantidadPersonas, int tipoRuta,
+            out float importeBruto, out float porcentajeDescuento, out float montoDescuento, out float importePago)
+        {
+            float[] precios = { 100, 120, 150 };
+
+            float precioPorPersona = precios[tipoRuta - 1];
+
+            importeBruto = cantidadPersonas * precioPorPersona;
+
+            if (cantidadPersonas == 1)
+            {
+                porcentajeDescuento = 0;
+            }
+            else if (cantidadPersonas >= 2 && cantidadPersonas <= 7)
+            {
+                porcentajeDescuento = 8;
+            }
+            else if (cantidadPersonas >= 8 && cantidadPersonas <= 16)
+            {
+                porcentajeDescuento = 13;
+            }
+            else
+            {
+                porcentajeDescuento = 15;
+            }
+
+            switch (tipoCliente)
+            {
+                case 'A': 
+                    porcentajeDescuento += 7;
+                    break;
+                case 'B': 
+                    porcentajeDescuento += 5;
+                    break;
+                default:
+                    break;
+            }
+
+            montoDescuento = (importeBruto * porcentajeDescuento) / 100;
+
+            importePago = importeBruto - montoDescuento;
+        }
+    }
+
+}
--- logica negocio/CalculadoraVentas.cs
+++ logica negocio/CalculadoraVentas.cs
@@ -15,6 +15,24 @@ public class CalculadoraVentas
 
         float precioPorPersona = precios[tipoRuta - 1];
 
+        importeBruto = cantidadPersonas * precioPorPersona;
+
+        if (cantidadPersonas == 1)
+        {
+            porcentajeDescuento = 0;
+        }
+        else if (cantidadPersonas >= 2 && cantidadPersonas <= 7)
+        {
+            porcentajeDescuento = 8;
+        }
+        else if (cantidadPersonas >= 8 && cantidadPersonas <= 16)
+        {
+            porcentajeDescuento = 13;
+        }
+        else
+        {
+            porcentajeDescuento = 15;
+        }
         
     }
 }
