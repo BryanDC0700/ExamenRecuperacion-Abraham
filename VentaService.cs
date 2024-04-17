abc1ce10a51e827442f2dbc3642d4c74e0ace5ab
Abraham Brian Diaz Condori <202013239d@utea.edu.pe>
2024-04-17 11:51:13 -0500

    error de carpetas

---
+++ VentaService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ExamenRecuperacion_Abraham
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "VentaService" en el código y en el archivo de configuración a la vez.
+    public class VentaService : IVentaService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
